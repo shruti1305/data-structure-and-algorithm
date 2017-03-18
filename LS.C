f6571e41accaaf8e4ad8582e52843034fe3a3921
shruti1305 <shrutijha.1305@gmail.com>
2017-03-18 13:16:51 +0530

    linear searcf

---
+++ LS.C
@@ -0,0 +1,24 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+ int a[15],n,m,c=0,i;
+ clrscr();
+ printf("enter the sizeof the elements");
+ scanf("%d",&n);
+ printf("enter elements to display");
+ for(i=0;i<=n-1;i++)
+ scanf("%d",&a[i]);
+ printf("enter no to be searched");
+ scanf("%d",&m);
+ for(i=0;i<=n-1;i++)
+ if(a[i]==m)
+ { c=1;
+   break;
+   }
+   if(c==0)
+   printf("no is nt found");
+   else
+   printf("no is found");
+   getch();
+   }
